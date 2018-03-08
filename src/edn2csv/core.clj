8dc250337468e7bc7af6f37b7aaf40e64569d589
M1NN <score@morris.umn.edu>
2018-03-07 18:00:09 -0600

    Added ParentOf_Edges file conversion

--- src/edn2csv/core.clj
+++ src/edn2csv/core.clj
@@ -9,6 +9,8 @@
 
 ; The header line for the Individuals CSV file
 (def individuals-header-line "UUID:ID(Individual),Generation:int,Location:int,:LABEL")
+(def semantics-header-line "UUID:ID(Semantics),TotalError:int,:LABEL")
+(def parentOf_edges-header-line ":START_ID(Individual),GeneticOperator,:END_ID(Individual),:TYPE")
 
 ; Ignores (i.e., returns nil) any EDN entries that don't have the
 ; 'clojure/individual tag.
@@ -37,34 +39,57 @@
     (apply safe-println csv-file $))
   1)
 
-(defn edn->csv-sequential [edn-file csv-file]
-  (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
-    (->>
-      (line-seq (io/reader edn-file))
-      ; Skip the first line because it's not an individual
-      (drop 1)
-      (map (partial edn/read-string {:default individual-reader}))
-      (map (partial print-individual-to-csv out-file))
-      (reduce +)
-      )))
+(defn print-parent-of-edges-to-csv
+  [csv-file line]
+  (let [parents (get line :parent-uuids)]
+    (dorun (map (fn [single-parent]
+      (as-> line $
+        (assoc $ :single-parent single-parent)
+        (map $ [:parent-uuids :genetic-operators :uuid])
+        (concat $ ["PARENT_OF"])
+        (apply safe-println csv-file $))) parents))
+  1))
 
-(defn edn->csv-pmap [edn-file csv-file]
-  (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
-    (->>
-      (line-seq (io/reader edn-file))
+;(defn edn->csv-sequential [edn-file csv-file]
+  ;(with-open [out-file (io/writer csv-file)]
+    ;(safe-println out-file individuals-header-line)Individual
+    ;(->>
+      ;(line-seq (io/reader edn-file))Individual
       ; Skip the first line because it's not an individual
-      (drop 1)
-      (pmap (fn [line]
-        (print-individual-to-csv out-file (edn/read-string {:default individual-reader} line))
-        1))
-      count
-      )))
+      ;(drop 1)
+      ;(map (partial edn/read-string {:default individual-reader}))
+      ;(map (partial print-individual-to-csv out-file))
+      ;(reduce +)
+      ;)))
+
+;(defn edn->csv-pmap [edn-file csv-file type]
+  ;(with-open [out-file (io/writer csv-file)]
+  ;(if type
+    ;(str "individual" type)
+    ;(safe-println out-file individuals-header-line)
+    ;(->>
+      ;(line-seq (io/reader edn-file))
+      ; Skip the first line because it's not an individual
+      ;(drop 1)
+      ;(pmap (fn [line]
+        ;(print-individual-to-csv out-file (edn/read-string {:default individual-reader} line))
+        ;1))
+    ;  count
+    ;  )))
+      ;ParentOf_Edge
+    ;(safe-println out-file parentOf_edges-header-line)
+    ;(->>
+      ;(line-seq (io/reader edn-file))
+      ;(drop 1)
+      ;(pmap (fn [line]
+      ;  (print-parent-of-edges-to-csv out-file (edn/read-string {:default individual-reader} line))
+      ;  1))
+      ;  count
+      ;  ))))
 
 (defn edn->csv-reducers [edn-file csv-file]
   (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
+    (safe-println out-file parentOf_edges-header-line)
     (->>
       (iota/seq edn-file)
       (r/map (partial edn/read-string {:default individual-reader}))
@@ -74,7 +99,7 @@
       ; to catch it. We could do that with `r/drop`, but that
       ; totally kills the parallelism. :-(
       (r/filter identity)
-      (r/map (partial print-individual-to-csv out-file))
+      (r/map (partial print-parent-of-edges-to-csv out-file))
       (r/fold +)
       )))
 
@@ -88,15 +113,26 @@
          "_sequential")
        "_Individuals.csv"))
 
+(defn build-parentOf-csv-filename
+  [edn-filename strategy]
+  (str (fs/parent edn-filename)
+        "/"
+        (fs/base-name edn-filename ".edn")
+        ;(if strategy
+          ;(str "_" strategy)
+          ;"_sequential")
+        "_ParentOf_edges.csv"))
+
+
 (defn -main
   [edn-filename & [strategy]]
-  (let [individual-csv-file (build-individual-csv-filename edn-filename strategy)]
+  (let [parentOf-csv-file (build-parentOf-csv-filename edn-filename strategy)]
     (time
       (condp = strategy
-        "sequential" (edn->csv-sequential edn-filename individual-csv-file)
-        "pmap" (edn->csv-pmap edn-filename individual-csv-file)
-        "reducers" (edn->csv-reducers edn-filename individual-csv-file)
-        (edn->csv-sequential edn-filename individual-csv-file))))
+        ;"sequential" (edn->csv-sequential edn-filename individual-csv-file)
+        ;"pmap" (edn->csv-pmap edn-filename individual-csv-file)
+        ;"reducers" (edn->csv-reducers edn-filename individual-csv-file)
+        (edn->csv-reducers edn-filename parentOf-csv-file))))
   ; Necessary to get threads spun up by `pmap` to shutdown so you get
   ; your prompt back right away when using `lein run`.
   (shutdown-agents))
